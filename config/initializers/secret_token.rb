03051fb5d4e5b74ef5ba3644aba3e9b7df8c3f57
Andre Williams <andrewilliamsyo@gmail.com>
2013-04-13 10:28:34 -0700

    New Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomePub::Application.config.secret_token = 'c022d6ca03f8dcc79c8d377efd1e115152cec0de53ddb6fda5d7024c919232177e03545fef1828161097b76ceb9bbd78ca026b53cb3eaf8b556d70bc382546b1'
